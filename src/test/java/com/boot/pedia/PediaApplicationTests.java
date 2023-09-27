05c0c489a0d4e71e96cee199adffc20eb651bf1b
origin <tkdrbs0802@naver.com>
2023-09-27 15:08:09 +0900

    init: clone project

---
+++ src/test/java/com/boot/pedia/PediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.pedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PediaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
