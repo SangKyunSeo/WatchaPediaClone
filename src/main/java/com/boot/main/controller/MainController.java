90c47d1b1387a37c58d1271b49a8b6384de02571
origin <tkdrbs0802@naver.com>
2023-10-12 18:35:44 +0900

    init: spring security

--- src/main/java/com/boot/main/controller/MainController.java
+++ src/main/java/com/boot/main/controller/MainController.java
@@ -1,14 +1,25 @@
 package com.boot.main.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RestController;
 
 @Controller
+@Slf4j
 public class MainController {
+
+    @GetMapping("/")
+    public String getHome(){
+        return "redirect:/main";
+    }
 
     @GetMapping("/main")
     public String getMain(){
-        return "main/main";
+        log.info("main으로 들어옴");
+        return "index";
     }
+
+
 
 }
